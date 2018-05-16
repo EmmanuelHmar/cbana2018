c55f2b9de5acbf1baf65ee827628f95cbd833927
EmmanuelHmar <eman.hmar@gmail.com>
2018-05-16 04:42:05 -0600

    fixed 4 so far

--- app/src/main/java/vanmang/com/Control_Page/Leaderships/CBA_Christian.java
+++ app/src/main/java/vanmang/com/Control_Page/Leaderships/CBA_Christian.java
@@ -1,15 +1,12 @@
 package vanmang.com.Control_Page.Leaderships;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.support.v7.widget.Toolbar;
 
 import vanmang.com.Control_Page.BottomNavigationIntents;
 import vanmang.com.Control_Page.R;
-import vanmang.com.Control_Page.Tabs.ActivityLeaderships;
 
 public class CBA_Christian extends AppCompatActivity {
 
@@ -19,15 +16,13 @@ public class CBA_Christian extends AppCompatActivity {
         setContentView(R.layout.activity_cba_christian);
 
 
-       ImageView button = (ImageView) findViewById(R.id.backButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CBA_Christian.this, ActivityLeaderships.class);
-                startActivity(intent);
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
 
+        toolbar.setTitle("Christian Education");
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //To populate the bottom navigation menu on the layout
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav_Bar);
